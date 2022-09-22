5e0b60ebb276cd2827c433eea7020fd7b6369a73
Sadern Alwis <44372160+sadernalwis@users.noreply.github.com>
2022-09-22 21:12:53 +0530

    Delete hairNet directory

--- hairNet/Utilities/CurveOps/CurveOps.py
+++
@@ -1,6 +0,0 @@
-class SplineOps:
-
-    def is_bezier(obj):
-        return (obj.type == 'CURVE' and len(obj.data.splines) > 0
-            and  obj.data.splines[0].type == 'BEZIER' 
-            and  len(obj.data.splines[0].bezier_points) > 0)
